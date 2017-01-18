2b199f8d3fcddc72001a34c66eed210d952f07be
HasnAr <f.eng.hasn@gmail.com>
2017-01-17 23:06:23 -0800

    almostThere

--- powerOfTwo/powerOfTwo.js
+++ powerOfTwo/powerOfTwo.js
@@ -3,7 +3,17 @@
 Calculate the sum of the digits of the number 2^1000?
 Hint the answer is 1366.
 */
-
+//O(n^n)
 var powerOfTwo = function(num){
-
+	debugger;
+	if(num<=69){
+		var temp = Math.pow(2,num).toString().split("");
+		return temp.reduce(function(a,b){
+			return Number(a)+ Number(b);
+		},0);
+	}
+	else
+		return powerOfTwo(num-69) +Math.pow(2,69).toString().split('').reduce(function(a,b){
+			return Number(a) + Number(b);
+		},0) ;  
 }
