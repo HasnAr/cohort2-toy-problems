f719ece461fc6a5c3c637afb46d499e51fa5dd71
HasnAr <f.eng.hasn@gmail.com>
2017-01-21 23:35:24 -0800

    almostThere

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -21,6 +21,40 @@ var solvedBoard = [[0,1,1],
 ticTacToe(solvedBoard);//should return 1
 */
 
+
+// k in case every was invoked 
+// n array length
+// o(nk)
 function ticTacToe(board){
 	
+	// check for minor or major win for x & o
+	if(board[0][0] ===1 && board[1][1] ===1 board[2][2] ===1 || board[0][2] ===1 && board[1][1] ===1 && board[2][0] ===1){
+		return 1;}
+	 if(board[0][0] ===2 && board[1][1] ===2 board[2][2] ===2 || board[0][2] ===2 && board[1][1] ===2 && board[2][0] ===2)	{
+		return 1;}
+	//itereate over the array of arrays to check for winner on rows prespective or col prespective
+	for(var i=0;i< board.length; i++){
+			// check if the whole row has all 1's
+			if(_.every(board[i], elem => elem === 1)){
+				return 1;
+			}
+			// check if the whole row has all 2's
+			else if(_.every(board[i], elem => elem === 2)){
+				return 1;
+			}
+			// check if the whole col has all 1's
+			else if(board[0][i] === 1 && board[1][i]=== 1 && board[2][i]=== 1){
+				return 1;
+			}
+			// check if the whole row has all 2's	
+			else if(board[0][i] === 2 && board[1][i]=== 2 && board[2][i]=== 2){
+				return 1;	
+			}
+	}
+	// else return -1
+	return -1;
 };
+
+
+
+
