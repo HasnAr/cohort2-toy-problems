b98eac64b70fbbb09b04e938503a0f6af2bbaed5
HasnAr <f.eng.hasn@gmail.com>
2017-01-26 07:06:09 -0800

    solved

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,25 @@
  * console.log(index); // 4
  */
 
+
+
+//o(logn)
+
 var binarySearch = function (array, target) {
-  
+	//iterate over the array 
+  for(var n=0;n<array.length;n++){
+  	//check if first element equal target
+  	if(target === array[n])
+  		return n;
+  	//check if last element equal target
+  	else if(target === array[array.length-1])
+  		return array.length-1;
+  	//if target > from elemnt in the midle of the array call binarysearch for the right half of the array
+  	else if(target> array[Math.ceil(array.length/2)])
+  		return binarySearch(array.splice(Math.ceil(array.length/2)),target) + n;
+  	//if target < from elemnt in the midle of the array call binarysearch for the left half of the array
+  	else if (target<array[Math.ceil(array.length/2)])
+  		return binarySearch(array.splice(0,Math.ceil(array.length/2)),target) + n;	
+  }
 };
 
